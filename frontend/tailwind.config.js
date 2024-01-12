bfd649ba3e51398fb5e026888c916387e4dd0987
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-12 14:07:50 +0200

    New code is now added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
+/* global require */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
-

467637f8b4cbfe848985b9b94fa116bbb241523c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 10:09:02 +0200

    nEW CODE IS NOW ADDED

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,7 +1,12 @@
+import data_product from "../Assets/data"
+
 
 export default function Popular() {
   return (
     <div>
+       <div className="">
+        <h1>POPULAR IN WOMEN</h1>
+       </div>
       
     </div>
   )

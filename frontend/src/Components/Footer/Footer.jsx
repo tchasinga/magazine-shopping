ee1f40ffd9312838d2c5f8192d6fdb8eb3845310
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 16:53:12 +0200

    New code is added

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
-
+import {SiGithub , SiShopify , SiLinkedin , SiInstagram , } from 'react-icons/si';
+import '../../index.css'
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className='mgenBg'>
+        <div className="flex justify-between items-center fixed mt-10 bottom-0 right-0 max-w-6xl mx-auto left-0 text-white py-5 px-10">
+            <div className="flex gap-5 items-center text-black">
+                <SiShopify className="text-4xl"/>
+                <h1 className="text-xl ">Shopify</h1>
+            </div>
+            <div className="flex gap-5 text-gray-800">
+                <p className=''>@Copyright 2024 all deserved made by tchasinga balolebwami </p>
+            </div>
+            <div className="flex gap-5 text-black items-center">
+                <SiGithub className="text-xl"/>
+                <SiLinkedin className="text-xl"/>
+                <SiInstagram className="text-xl"/>
+            </div>
+        </div>
     </div>
   )
 }
--- frontend/src/Components/NewLetter/NewsLetter.jsx
+++ frontend/src/Components/NewLetter/NewsLetter.jsx
@@ -1,8 +1,8 @@
 
 export default function NewsLetter() {
   return (
-    <div>
-        <div className="flex flex-col  w-full items-center justify-center h-[50vh] bg-slate-100 my-10 rounded-xl">
+    <div className="pb-20">
+        <div className="flex flex-col  w-full items-center justify-center h-[50vh] bg-slate-100 my-10 rounded-xl ">
              <div className="text-5xl pb-3 text-slate-950">
                     <h1 className="">Subscribe to our offer on email</h1>   
              </div>

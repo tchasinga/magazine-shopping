095ba3bfd99a77051413e29be431c15fb2727b8a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 11:50:46 +0200

    new code is added

--- frontend/src/Components/NewLetter/NewsLetter.jsx
+++ frontend/src/Components/NewLetter/NewsLetter.jsx
@@ -2,7 +2,24 @@
 export default function NewsLetter() {
   return (
     <div>
-      
+        <div className="">
+             <div className="">
+                    <h1 className="">Subscribe to our Offer On Email</h1>   
+             </div>
+
+             <div className="">
+             <p className="">Get notification about new arrivals in your inbox.</p>
+             </div>
+
+             <div className="">
+                <input type="text" id="text" className=""/>
+
+                <div className="">
+                    <button className="">Subscribe</button>
+                </div>
+                
+             </div>
+        </div>
     </div>
   )
 }

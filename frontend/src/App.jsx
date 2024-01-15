4536d9a701abd1db499e9fd5fa8b21bf01b362df
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 08:55:52 +0200

    New code is not added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import Navbars from "./Components/Navbar/Navbars"
+import Navbars from "./Components/Navbar/Navbars";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Shop from "./Pages/Shop";
 
 function App() {
 
   return (
     <div className="max-w-6xl mx-auto p-4">
+      <BrowserRouter>
       <Navbars />
+      <Routes>
+        <Route path="/" element={<Shop/>} />
+        <Route path="/mens" element={<Shop/>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+       </Routes>
+      </BrowserRouter>
     </div>
   )
 }

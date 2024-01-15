90f05802773814ae2248c1dbddd7a817ca5e56e6
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 09:03:33 +0200

    New code is

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Navbars from "./Components/Navbar/Navbars";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Shop from "./Pages/Shop";
 import ShoppingKinds from "./Pages/ShoppingKinds";
+import Products from "./Pages/Products";
 
 function App() {
 
@@ -14,6 +15,11 @@ function App() {
         <Route path="/mens" element={<ShoppingKinds category="mens"/>} />
         <Route path="/womens" element={<ShoppingKinds category="womens"/>} />
         <Route path="/child" element={<ShoppingKinds category="child"/>} />
+
+        <Route path="/product" element={<Products/>}>
+          
+        </Route>
+
        </Routes>
       </BrowserRouter>
     </div>

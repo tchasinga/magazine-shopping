d888e901b99218cb3e03d104b066678695edc730
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 09:40:38 +0200

    new commit

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -6,10 +6,10 @@ export default function Hero() {
 
         <div className="flex items-center justify-between">
 
-            <div className="flex flex-col border">
-                <h1 className="font-bold text-slate-900 text-3xl">Welcom shopify</h1>
+            <div className="flex flex-col">
+                <h1 className="font-bold text-slate-900 text-3xl">Welcom to shopify</h1>
                 <h1 className="font-bold text-slate-900 text-lg">We sell great things</h1>
-                <div className="text-sm font-normal text-gray-900">
+                <div className="text-sm font-normal text-gray-900 my-5">
                 <h1 className="">Welcom to the shoppify quality we sell good things</h1>
                 <h2 className="">Find more product with less price and good currency statement...</h2>
                 </div>
@@ -18,7 +18,7 @@ export default function Hero() {
 
             <div className="">
                 <div className="">
-                    <img className="object-fill" src={ImgJordan} alt="" />
+                    <img className="object-fill -rotate-12" src={ImgJordan} alt="" />
                 </div>
             </div>
 

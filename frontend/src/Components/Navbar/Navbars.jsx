34b1f213c7cd414deceffc0962da9c6241e82524
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 08:36:48 +0200

    new code is added now

--- frontend/src/Components/Navbar/Navbars.jsx
+++ frontend/src/Components/Navbar/Navbars.jsx
@@ -12,8 +12,8 @@ export default function Navbars() {
             <h1 className="text-2xl text-center pt-3 font-bold text-slate-900">Shopify</h1>
            </div>
 
-           <ul className="flex items-center gap-3 text-slate-800 text-[15px]">
-            <li className="">Shop</li>
+           <ul className="nav-menu flex items-center gap-3 text-slate-800 text-[15px]">
+            <li className="">Shop <hr /></li>
             <li className="">Mens</li>
             <li className="">Womens</li>
             <li className="">Child</li>

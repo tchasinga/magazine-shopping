b5a9f19993190c09096a5605534d5aa3eafd52e4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 10:50:31 +0200

    New code is added now

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,4 +1,5 @@
 import Hero from "../Components/Hero/Hero";
+import Offer from "../Components/Offer/Offer";
 import Popular from "../Components/Popular/Popular";
 
 export default function Shop() {
@@ -6,6 +7,7 @@ export default function Shop() {
     <div>
       <Hero />
       <Popular />
+      <Offer />
     </div>
   )
 }

bed46ef6a625d63ab8a1413909da6108b9c95cd1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 10:05:20 +0200

    New code

--- frontend/src/Components/Item/Items.jsx
+++ frontend/src/Components/Item/Items.jsx
@@ -1,8 +1,37 @@
+import PropTypes from 'prop-types';
 
-export default function Items() {
+export default function Items(props) {
   return (
     <div>
-      
+     
+                <div className="mygridreponsive">
+                    <div className="">
+                        <div>
+                            <img src={props.image} alt="" />
+                        </div>
+
+                        <div>
+                            <p>{props.name}</p>
+                        </div>
+
+                        <div>
+                            <p>{props.new_price}</p>
+                        </div>
+
+                        <div>
+                            <p>{props.old_price}</p>
+                        </div>
+                    </div>
+                    
+                </div>
     </div>
   )
 }
+
+
+Items.propTypes = {
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    new_price: PropTypes.string.isRequired,
+    old_price: PropTypes.string.isRequired,
+};

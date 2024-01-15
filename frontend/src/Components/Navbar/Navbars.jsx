ffec870190d03b81fa5a8f837752a7b510ae1e9d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 08:30:29 +0200

    New design is white !important;

--- frontend/src/Components/Navbar/Navbars.jsx
+++ frontend/src/Components/Navbar/Navbars.jsx
@@ -1,29 +1,30 @@
 import {SiShopify,  } from 'react-icons/si';
-import { MdAddShoppingCart } from "react-icons/md";
+import { PiShoppingCartLight } from "react-icons/pi";
 import '../../index.css'
 
 export default function Navbars() {
   return (
     <div>
-        <div className="">
+        <div className="flex items-center justify-between">
 
            <div className="flex  gap-2 items-center">
             <SiShopify className='text-4xl'/>
             <h1 className="text-2xl text-center pt-3 font-bold text-slate-900">Shopify</h1>
            </div>
 
-           <ul className="">
+           <ul className="flex items-center gap-3 text-slate-800 text-[15px]">
             <li className="">Shop</li>
             <li className="">Mens</li>
             <li className="">Womens</li>
             <li className="">Child</li>
            </ul>
 
-           <div className="">
-              <MdAddShoppingCart />
+           <div className="relative flex items-center gap-3">
+              <PiShoppingCartLight className='text-2xl'/>
+              <div className="nav-cart-count">0</div>
               <button>Loging</button>
            </div>
-           
+
         </div>
     </div>
   )

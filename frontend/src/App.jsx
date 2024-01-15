d4c6c55ce266f57d858b792fb177e66490497eed
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 08:07:22 +0200

    New code commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import Navbars from "./Components/Navbar/Navbars"
 function App() {
 
   return (
-    <>
+    <div className="max-w-6xl mx-auto p-4">
       <Navbars />
-    </>
+    </div>
   )
 }
 
--- frontend/src/Components/Navbar/Navbars.jsx
+++ frontend/src/Components/Navbar/Navbars.jsx
@@ -1,6 +1,17 @@
+import {SiShopify} from 'react-icons/si';
+import '../../index.css'
+
 export default function Navbars() {
   return (
     <div>
+        <div className="">
+
+           <div className="flex  gap-2 items-center">
+            <SiShopify className='text-4xl'/>
+            <h1 className="text-2xl text-center pt-3 font-bold text-slate-900">Shopify</h1>
+           </div>
+
+        </div>
     </div>
   )
 }

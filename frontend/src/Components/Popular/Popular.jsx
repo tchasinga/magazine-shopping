aa26afd314a9b87173bf5f8e20d061ef599f931d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 10:11:57 +0200

    new commit

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,4 +1,5 @@
 import data_product from "../Assets/data"
+import Items from "../Item/Items"
 
 
 export default function Popular() {
@@ -7,7 +8,12 @@ export default function Popular() {
        <div className="">
         <h1>POPULAR IN WOMEN</h1>
        </div>
-      
+
+       <div className="">
+        {data_product.map((item) =>(
+            return <Items key={item.id} image={item.image} name={item.name} new_price={item.new_price} old_price={item.old_price} />
+        ))}
+       </div>
     </div>
   )
 }

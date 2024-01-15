d5f9bc630c65bbbdb88027ee94e2aff615146e2f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 08:15:19 +0200

    new commit

--- frontend/src/Components/Navbar/Navbars.jsx
+++ frontend/src/Components/Navbar/Navbars.jsx
@@ -1,4 +1,5 @@
-import {SiShopify} from 'react-icons/si';
+import {SiShopify,  } from 'react-icons/si';
+import { MdAddShoppingCart } from "react-icons/md";
 import '../../index.css'
 
 export default function Navbars() {
@@ -11,6 +12,18 @@ export default function Navbars() {
             <h1 className="text-2xl text-center pt-3 font-bold text-slate-900">Shopify</h1>
            </div>
 
+           <ul className="">
+            <li className="">Shop</li>
+            <li className="">Mens</li>
+            <li className="">Womens</li>
+            <li className="">Child</li>
+           </ul>
+
+           <div className="">
+              <MdAddShoppingCart />
+              <button>Loging</button>
+           </div>
+           
         </div>
     </div>
   )

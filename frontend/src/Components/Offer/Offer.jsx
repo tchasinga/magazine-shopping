9f7d81078d815aafefb2d0f0ab7331a3cd680292
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 10:55:52 +0200

    New code is online

--- frontend/src/Components/Offer/Offer.jsx
+++ frontend/src/Components/Offer/Offer.jsx
@@ -1,8 +1,19 @@
+import exclusive from '../../img/AppleAirPods.png'
+
 
 export default function Offer() {
   return (
     <div>
-      
+      <div className="">
+        <h1>Exclusive</h1>
+        <h2>Offer For You</h2>
+        <p>Get 30% Off On First Order</p>
+        <button>Shop Now</button>
+      </div>
+
+      <div className="">
+        <img src={exclusive} alt="" />
+      </div>
     </div>
   )
 }

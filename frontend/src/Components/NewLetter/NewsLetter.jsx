2b419a5964d69f81e8e7234a2c8248e912882dc7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 11:58:58 +0200

    New

--- frontend/src/Components/NewLetter/NewsLetter.jsx
+++ frontend/src/Components/NewLetter/NewsLetter.jsx
@@ -2,22 +2,24 @@
 export default function NewsLetter() {
   return (
     <div>
-        <div className="">
-             <div className="">
-                    <h1 className="">Subscribe to our Offer On Email</h1>   
+        <div className="flex flex-col  w-full items-center justify-center h-[50vh] bg-slate-100 my-10 rounded-xl">
+             <div className="text-5xl pb-3 text-slate-950">
+                    <h1 className="">Subscribe to our offer on email</h1>   
              </div>
 
-             <div className="">
+             <div className="text-1xl pb-3 text-slate-950">
              <p className="">Get notification about new arrivals in your inbox.</p>
              </div>
 
              <div className="">
-                <input type="text" id="text" className=""/>
+               <div className="bordering">
+               <input type="text" id="text" className=""/>
+               </div>
 
                 <div className="">
                     <button className="">Subscribe</button>
                 </div>
-                
+
              </div>
         </div>
     </div>
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,4 +1,5 @@
 import Hero from "../Components/Hero/Hero";
+import NewsLetter from "../Components/NewLetter/NewsLetter";
 import Offer from "../Components/Offer/Offer";
 import Popular from "../Components/Popular/Popular";
 import NewCollections from "../Components/newCollections/NewCollections";
@@ -10,6 +11,7 @@ export default function Shop() {
       <Popular />
       <Offer />
       <NewCollections />
+      <NewsLetter />
     </div>
   )
 }

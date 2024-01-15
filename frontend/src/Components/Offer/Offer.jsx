460801597e9f25fd9632b4320d57b04b81d482c3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 11:07:41 +0200

    new code

--- frontend/src/Components/Offer/Offer.jsx
+++ frontend/src/Components/Offer/Offer.jsx
@@ -6,8 +6,8 @@ export default function Offer() {
     <div className='flex items-center justify-between'>
       <div className="">
         <h1 className='text-3xl'>Exclusive</h1>
-        <p className='font-medium text-lg'>Get 30% Off On First Order</p>
-        <h2 className='text-sm'>We get an offer for You here come and pick your goods</h2>
+        <p className='font-medium text-lg pt-3'>Get 30% Off On First Order</p>
+        <h2 className='text-sm pb-5'>We get an offer for You here come and pick your goods</h2>
        
        <div className="bg-slate-100 py-2 px-5 w-32 rounded-r-3xl border">
        <button className='text-sm font-medium'>Shop Now</button>

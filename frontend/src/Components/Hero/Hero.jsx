0175f1177b89792a963ab3fe64052b744142db74
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-15 09:33:28 +0200

    New code is added

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,9 +2,22 @@
 export default function Hero() {
   return (
     <div>
+
         <div className="">
-            
+
+            <div className="flex flex-col w-full">
+                <h1 className="font-bold text-slate-900 text-3xl">Welcom shopify</h1>
+                <h1 className="font-bold text-slate-900 text-lg">We sell great things</h1>
+                <div className="text-sm font-normal text-gray-900">
+                <h1 className="">Welcom to the shoppify quality we sell good things</h1>
+                <h2 className="">Find more product with less price and good currency statement...</h2>
+                </div>
+                <p className="text-xs text-red-950 font-medium">Let&apos;us made shopping</p>
+            </div>
+
+            <div className=""></div>
         </div>
+
     </div>
   )
 }

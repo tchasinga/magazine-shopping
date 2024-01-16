7758fa8ace9f1026c634f6e318cf30d481fd587f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-16 11:16:10 +0200

    New commit is now added

--- frontend/src/Contexts/ShopContext.jsx
+++ frontend/src/Contexts/ShopContext.jsx
@@ -1,1 +1,21 @@
+import PropTypes from "prop-types";
+import { createContext } from "react";
+import all_product from '../Components/Assets/all_product.js';
 
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+    const contextValue = all_product;
+
+    return (
+        <ShopContext.Provider value={{ contextValue }}>
+            {props.children}
+        </ShopContext.Provider>
+    );
+};
+
+ShopContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default ShopContextProvider;

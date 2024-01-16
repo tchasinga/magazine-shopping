607241186eaca6c53d814bb56dccfb2856264c71
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-16 11:23:13 +0200

    New code

--- frontend/src/Pages/ShoppingKinds.jsx
+++ frontend/src/Pages/ShoppingKinds.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react"
 
-export default function ShoppingKinds() {
+export default function ShoppingKinds(props) {
+
+  const {all_product} = useContext(ShopContext);
+
   return (
     <div>
       

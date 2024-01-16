3ff686c935983e078996bc50e6433156b26ff419
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-16 14:54:15 +0200

    mew code is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbars from "./Components/Navbar/Navbars";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
 import ShoppingKinds from "./Pages/ShoppingKinds";
 import Products from "./Pages/Products";
@@ -11,29 +11,36 @@ import women_banner from "./Components/Assets/banner_women.png";
 import child_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
-
   return (
     <div className="max-w-6xl mx-auto p-4">
       <BrowserRouter>
-      <Navbars />
-      <Routes>
-        <Route path="/" element={<Shop/>} />
-        <Route path="/mens" element={<ShoppingKinds banner={men_banner} category="men"/>} />
-        <Route path="/womens" element={<ShoppingKinds banner={women_banner} category="women"/>} />
-        <Route path="/child" element={<ShoppingKinds banner={child_banner} category="kid"/>} />
+        <Navbars />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShoppingKinds banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShoppingKinds banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/child"
+            element={<ShoppingKinds banner={child_banner} category="kid" />}
+          />
 
-        <Route path="/product" element={<Products/>}>
-          <Route path=":productId" element={<Products/>} />
-        </Route>
+          <Route path="/product" element={<Products />}>
+            <Route path=":productId" element={<Products />} />
+          </Route>
 
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/login" element={<LoginSignup/>} />
-
-       </Routes>
-        <Footer  />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

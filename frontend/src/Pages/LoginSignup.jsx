fb52231ba904e6c03dbbc4972bfd5194d73a1c17
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-16 13:08:17 +0200

    New code is added

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,17 @@
 
 export default function LoginSignup() {
   return (
-    <div>
-      
+    <div className="loginSingup">
+      <div className="loginsignup-container">
+         <h1>Sign Up</h1>
+         <div className="loginSignup-fields">
+            <input type="text" placeholder="First Name" />
+            <input type="email" placeholder="Email adress" />
+            <input type="password" placeholder="Password" />
+         </div>
+          <button>Sign Up</button>
+          <p className="loginSignup-login">Already have an account</p>
+      </div>
     </div>
   )
 }

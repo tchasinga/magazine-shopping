64bc10b3e77b36c02354d405ee4ca634ac7e4ac0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-16 16:46:26 +0200

    New code is commited now

--- frontend/src/Components/Related/Related.jsx
+++ frontend/src/Components/Related/Related.jsx
@@ -1,7 +1,26 @@
+import data_product from "../Assets/data"
+import Items from "../Item/Items"
+
 
 export default function Related() {
   return (
-    <div>      
+    <div>  
+        <div className="related">
+            <h1 className="text-4xl mt-20 font-bold text-slate-950">Related Products </h1>
+            <hr  className="mt-2 w-32"/>
+            <div className="mygridreponsive mt-10 mb-20">
+            {data_product.map((items , i) => (
+          <Items
+            key={i}
+            id={items.id}
+            image={items.image}
+            name={items.name}
+            new_price={items.new_price}
+            old_price={items.old_price}
+          />
+        ))}
+            </div>
+        </div>    
     </div>
   )
 }
--- frontend/src/Pages/Products.jsx
+++ frontend/src/Pages/Products.jsx
@@ -3,6 +3,7 @@ import  {ShopContext}  from "../Contexts/ShopContext.jsx"
 import { useParams } from "react-router-dom"
 import Breadcrum from "../Components/Breadcrum/Breadcrum.jsx";
 import Displaydetails from "../Components/Displaydetails/Displaydetails.jsx";
+import Related from "../Components/Related/Related.jsx";
 
 export default function Products() {
 
@@ -14,6 +15,7 @@ export default function Products() {
     <div>
       <Breadcrum product={product} />
       <Displaydetails product={product} />
+      <Related />
     </div>
   )
 }

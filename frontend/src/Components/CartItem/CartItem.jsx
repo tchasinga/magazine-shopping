4e4d2f4962a7449eaa9d2c7fe605bc98a9114656
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-17 10:59:14 +0200

    New code is added now

--- frontend/src/Components/CartItem/CartItem.jsx
+++ frontend/src/Components/CartItem/CartItem.jsx
@@ -1,35 +1,37 @@
-import { useContext } from "react"
-import { ShopContext } from "../../Contexts/ShopContext"
+import { useContext } from "react";
+import { ShopContext } from "../../Contexts/ShopContext";
 import { MdDelete } from "react-icons/md";
-import {Tooltip} from '@mate'
-
+import { Tooltip } from "@mui/material";
 
 export default function CartItem() {
-
-    const {all_product , cartItems  , removeFromCart  } = useContext(ShopContext)
+  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className="cartitems">
-        <div className="cartitems-format-main">
-            <p>Products</p>
-            <p>Title</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Total</p>
-            <p>Remove</p>
-        </div>
-        <hr />
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
 
-        <div className="">
-            <div className="cartitems-format">
-              <img src="" alt="" className="carticon-product-icon"/>
-              <p></p>
-              <p></p>
-              <button className="cartitems-quantity"></button>
-              <p></p>
-              <MdDelete />
-            </div>
+      <div className="">
+        <div className="cartitems-format">
+          <img src="" alt="" className="carticon-product-icon" />
+          <p></p>
+          <p></p>
+          <button className="cartitems-quantity"></button>
+          <p></p>
+
+          <Tooltip title="click here to delete.." arrow placement="left" className="cursor-pointer">
+            <MdDelete className="cartitems-delete-icon" />
+          </Tooltip>
+
         </div>
+      </div>
     </div>
-  )
+  );
 }

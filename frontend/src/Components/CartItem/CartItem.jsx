cc10a0df07e3d7e3f6fe90f27b1bca0c7366d79a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-17 10:39:42 +0200

    New code commited

--- frontend/src/Components/CartItem/CartItem.jsx
+++ frontend/src/Components/CartItem/CartItem.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react"
+import { ShopContext } from "../../Contexts/ShopContext"
 
 
 export default function CartItem() {
+
+    const {all_product , cartItems  , removeFromCart  } = useContext(ShopContext)
+
   return (
     <div>
         

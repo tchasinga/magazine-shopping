de88e10cc7dff1b80135d2b467095770c7236c57
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-17 11:34:08 +0200

    new code is added now

--- frontend/src/Components/CartItem/CartItem.jsx
+++ frontend/src/Components/CartItem/CartItem.jsx
@@ -2,9 +2,10 @@ import { useContext } from "react";
 import { ShopContext } from "../../Contexts/ShopContext";
 import { MdDelete } from "react-icons/md";
 import { Tooltip } from "@mui/material";
+import PropTypes from 'prop-types';
 
 export default function CartItem() {
-  const {all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className="cartitems">
@@ -17,30 +18,43 @@ export default function CartItem() {
         <p>Remove</p>
       </div>
       <hr />
-
-      {all_product.map((e) => {
-        if (cartItems[e.id] > 0) {
+      {all_product.map((item) => {
+        if (cartItems[item] > 0) {
           return (
-            <div className="" key={e.id}>
-              <div className="cartitems-format">
-                <img src={e.image} alt="" className="carticon-product-icon" />
-                <p>{e.name}</p>
-                <p>${e.new_price}</p>
-                <button className="cartitems-quantity">{cartItems[e.id]}</button>
-                <p>{e.new_price * cartItems[e.id]}</p>
-
-                <Tooltip title="click here to edit your profile" arrow placement="left">
-                  <div>
-                    <MdDelete className="cartitems-delete-icon" onClick={() => removeFromCart(e.id)} />
-                  </div>
+            <div className="cartitems-format" key={item}>
+              <div className="cartitems-format-img">
+                <img src={all_product[item].image} alt="" />
+              </div>
+              <div className="cartitems-format-title">
+                <p>{all_product[item].name}</p>
+              </div>
+              <div className="cartitems-format-price">
+                <p>${all_product[item].new_price}</p>
+              </div>
+              <div className="cartitems-format-quantity">
+                <p>{cartItems[item]}</p>
+              </div>
+              <div className="cartitems-format-total">
+                <p>${all_product[item].new_price * cartItems[item]}</p>
+              </div>
+              <div className="cartitems-format-remove">
+                <Tooltip title="Remove">
+                  <MdDelete
+                    className="cartitems-format-remove-icon"
+                    onClick={() => removeFromCart(item)}
+                  />
                 </Tooltip>
               </div>
-              <hr />
             </div>
           );
+        } else {
+          return null;
         }
-        return null; // added to satisfy React's requirement for a return value in map
       })}
     </div>
   );
 }
+
+CartItem.propTypes = {
+  all_product: PropTypes.object.isRequired, // Adjusted PropTypes for all_product
+};

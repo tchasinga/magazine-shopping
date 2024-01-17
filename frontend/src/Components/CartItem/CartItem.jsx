1f3e95a34c1d71f225087da733882c815a7deb2d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-17 11:18:59 +0200

    New code is added now

--- frontend/src/Components/CartItem/CartItem.jsx
+++ frontend/src/Components/CartItem/CartItem.jsx
@@ -4,7 +4,7 @@ import { MdDelete } from "react-icons/md";
 import { Tooltip } from "@mui/material";
 
 export default function CartItem() {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const {all_product, cartItems, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className="cartitems">
@@ -18,7 +18,29 @@ export default function CartItem() {
       </div>
       <hr />
 
-     
+      {all_product.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div className="" key={e.id}>
+              <div className="cartitems-format">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitems-quantity">{cartItems[e.id]}</button>
+                <p>{e.new_price * cartItems[e.id]}</p>
+
+                <Tooltip title="click here to edit your profile" arrow placement="left">
+                  <div>
+                    <MdDelete className="cartitems-delete-icon" onClick={() => removeFromCart(e.id)} />
+                  </div>
+                </Tooltip>
+              </div>
+              <hr />
+            </div>
+          );
+        }
+        return null; // added to satisfy React's requirement for a return value in map
+      })}
     </div>
   );
 }
--- frontend/src/Contexts/ShopContext.jsx
+++ frontend/src/Contexts/ShopContext.jsx
@@ -1,41 +1,39 @@
 import PropTypes from "prop-types";
 import { createContext, useState } from "react";
-import all_product from '../Components/Assets/all_product';
+import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart = {}
-    for (let index = 0; index < all_product.length + 1; index++) {
-        cart[index] = 0
-    }
-    return cart
-}
-
+  let cart = {};
+  for (let index = 0; index < all_product.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
 
 const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    const [cartItems, setCartItems] = useState(getDefaultCart());
- 
-   const addToCart = (itemId) => {
-    setCartItems((prev)=> ({...prev, [itemId]:prev[itemId]+1}))
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     console.log(cartItems);
-   }
+  };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=> ({...prev, [itemId]:prev[itemId]-1}))
-    }
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
-    const contextValue = {all_product , cartItems  , removeFromCart , addToCart};
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    );
+  const contextValue = { all_product, cartItems, removeFromCart, addToCart };
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
 };
 
 ShopContextProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default ShopContextProvider;
